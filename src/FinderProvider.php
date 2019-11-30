7a7e87420d63e5854bd6c72458f6b288c4f898ce
Ricardo R Sierra <ricardo@sierratecnologia.com.br>
2019-11-30 10:45:36 -0300

    Include casa in provider

--- src/FinderProvider.php
+++ src/FinderProvider.php
@@ -14,6 +14,7 @@ class FinderProvider extends ServiceProvider
         \Finder\Providers\FinderRouteProvider::class,
 
         \Tracking\TrackingProvider::class,
+        \Casa\CasaProvider::class,
 
         /**
          * Externos
